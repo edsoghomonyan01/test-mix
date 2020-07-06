5e91bd474a90617ecb8b337c7273908044ec1a9e
Ed Soghomonyan <edsoghomonyan01@gmail.com>
2020-07-06 15:28:36 +0400

    install dynamic import

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,12 +6,13 @@ import Vue             from 'vue';
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
+// import ExampleComponent from './components/ExampleComponent.vue';
+const ExampleComponent = () => import('./components/ExampleComponent.vue');
 
 const app = new Vue({
     el: '#app',
     components: {
-        'example-component':   require('./components/ExampleComponent.vue').default,
+        'example-component':   ExampleComponent,
     },
 });
 
